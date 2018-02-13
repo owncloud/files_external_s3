059bedbcda60827d91355a5b2318c1c0bcb19074
Patrick Jahns <github@patrickjahns.de>
2018-02-13 21:08:11 +0100

    migrate unit tests from core

---
+++ tests/unit/config.sample.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+    'run'           => true,
+    'bucket'        => 'octesting',
+    'hostname'      => 'ceph',
+    'port'          => '80',
+    'key'           => 'owncloud123456',
+    'secret'        => 'secret123456',
+    'use_ssl'       => false,
+    'use_path_style'=> true,
+);
